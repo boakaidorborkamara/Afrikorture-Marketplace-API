27db385c4861dcf4905b2436b9aaa3faf2b4f032
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-12-13 09:24:24 +0000

    created a node server and added dummy routes

--- server.js
+++ server.js
@@ -3,9 +3,26 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Welcome to Afrikorture Marketplace');
+  console.log(req.url);
 })
+
+app.get('/vendors', (req,res) =>{
+    res.send("Loading vendors list.");
+    console.log(req.url);
+});
+
+app.get('/shops', (req,res) =>{
+    res.send("Loading shops list.");
+    console.log(req.url);
+});
+
+app.get('/products', (req,res) =>{
+    res.send("Loading products list.");
+    console.log(req.url);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
+
